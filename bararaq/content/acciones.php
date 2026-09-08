1d5b2915c3295b4f4da71d038f33f04866153240
Ruly <99045522+vcruly@users.noreply.github.com>
2026-09-08 12:39:36 -0400

    u

--- bararaq/content/acciones.php
+++ bararaq/content/acciones.php
@@ -1,7 +1,7 @@
 <div class="row justify-content-center mt-5">
 <div class="col-xxl-10">
 <div class="row">
-<div class="col-xl-9">
+<div class="col-12">
 <div class="card mb-1">
 
     <div class="card-body">
@@ -9,29 +9,32 @@
     <div class="flex-grow-1">
         <p class="text-muted text-uppercase mb-2 fw-semibold">Actualizacion de la aplicacion</p>
         <h4 class="fs-lg mb-2">
-            <a href="forum-post.html" class="link-reset">Should AI tools be regulated by governments?</a>
+            <a href="" class="link-reset"></a>
         </h4>
-        <p class="text-muted mb-0">
-            <?php echo $output; ?>
+        <p class="text-muted mb-0 mt-4">
+        <?php
+
+            if(!empty($output)){ print_r($output); }
+            else{ echo "No se pudo actualizar. Contacte al soporte o revise su coneccion a internet."; };
+
+        ?>
         </p>
     </div>
     </div>
     </div>
 
     <div class="card-footer">
-        <p class="d-flex flex-wrap gap-3 text-muted mb-0 align-items-center justify-content-between fs-sm">
-            <span class="d-flex align-items-center gap-2">
-                <img src="assets/images/users/user-8.jpg" alt="avatar-8" class="img-fluid avatar-sm rounded">
-                <span>
-                    <a href="#!" class="link-dark fw-semibold lh-sm d-block">Amira Lee</a>
-                    <span class="text-muted small">2 hours ago</span>
-                </span>
-            </span>
-            <span><i class="ti ti-message-reply"></i> <a href="#!" class="link-reset">Answers: 62</a></span>
-            <span><i class="ti ti-clock"></i> Ends in: 4 days</span>
-            <span><i class="ti ti-users"></i> Votes: 894</span>
-            <span class="d-flex align-items-center gap-1"><span class="badge text-bg-warning">23</span></span>
-        </p>
+    <p class="d-flex flex-wrap gap-3 text-muted mb-0 align-items-center justify-content-between fs-sm">
+        <span><i class="ti ti-message-reply"></i> <a href="#!" class="link-reset"><?php echo file_get_contents("v.txt"); ?></a></span>
+        <span><i class="ti ti-clock"></i> Hora: <?php echo date("h:i:s A"); ?></span>
+        <span><i class="ti ti-users"></i> Usuario: <?php echo $user_data["nombre"]; ?></span>
+        <?php
+
+            if(empty($output)){ echo "<span class='d-flex align-items-center gap-1'><span class='badge text-bg-danger'>ERROR</span></span>"; }
+            else{ echo "<span class='d-flex align-items-center gap-1'><span class='badge text-bg-success'>OK</span></span>"; }
+
+         ?>
+    </p>
     </div>
 
 </div>
