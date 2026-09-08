60512eefcebba008c3b31c9d0d18568eb0712fba
Ruly <99045522+vcruly@users.noreply.github.com>
2026-09-07 23:46:48 -0400

    u

--- actualizar.php
+++ actualizar.php
@@ -1,24 +1,9 @@
 <?php
 
-/*$bat = 'C:/GCIO/scripts/actualizar.bat';
+$bat = 'C:/GCIO/scripts/actualizar.bat';
 
 exec("cmd /c \"$bat\" 2>&1", $output);
-print_r($output);*/
-
-
-header('Content-Type: application/json; charset=utf-8');
-$updater = 'C:\\GCIO\\scripts\\updater.bat';
-
-if(!file_exists($updater)){
-
-    echo json_encode(['success' => false, 'message' => 'No se encontro el actualizador.']);
-    exit;
-}
-
-$command = 'start "" /B "' . $updater . '"';
-pclose(popen($command, 'r'));
-
-echo json_encode(['success' => true, 'message' => 'Actualizacion iniciada.']);
+print_r($output);
 
 
  ?>
---
+++ bararaq/content/acciones.php
@@ -0,0 +1,42 @@
+<div class="row justify-content-center mt-5">
+<div class="col-xxl-10">
+<div class="row">
+<div class="col-xl-9">
+<div class="card mb-1">
+
+    <div class="card-body">
+    <div class="d-flex gap-4 align-items-center">
+    <div class="flex-grow-1">
+        <p class="text-muted text-uppercase mb-2 fw-semibold">Actualizacion de la aplicacion</p>
+        <h4 class="fs-lg mb-2">
+            <a href="forum-post.html" class="link-reset">Should AI tools be regulated by governments?</a>
+        </h4>
+        <p class="text-muted mb-0">
+            <?php echo $output; ?>
+        </p>
+    </div>
+    </div>
+    </div>
+
+    <div class="card-footer">
+        <p class="d-flex flex-wrap gap-3 text-muted mb-0 align-items-center justify-content-between fs-sm">
+            <span class="d-flex align-items-center gap-2">
+                <img src="assets/images/users/user-8.jpg" alt="avatar-8" class="img-fluid avatar-sm rounded">
+                <span>
+                    <a href="#!" class="link-dark fw-semibold lh-sm d-block">Amira Lee</a>
+                    <span class="text-muted small">2 hours ago</span>
+                </span>
+            </span>
+            <span><i class="ti ti-message-reply"></i> <a href="#!" class="link-reset">Answers: 62</a></span>
+            <span><i class="ti ti-clock"></i> Ends in: 4 days</span>
+            <span><i class="ti ti-users"></i> Votes: 894</span>
+            <span class="d-flex align-items-center gap-1"><span class="badge text-bg-warning">23</span></span>
+        </p>
+    </div>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+
