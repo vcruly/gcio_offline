01f31adbd27ff38ae8e031ad244deac36fbadba6
Ruly <99045522+vcruly@users.noreply.github.com>
2026-09-08 13:29:43 -0400

    Update acceso.php

--- bararaq/resources/libraries/seguridad/acceso.php
+++ bararaq/resources/libraries/seguridad/acceso.php
@@ -19,7 +19,7 @@ function login($email, $password) {
 
             setcookie(SITE_COOKIE, $user_data["id"], [
                 "expires" => time() + (9999 * 24 * 60 * 60),
-                "path" => "/"
+                "path" => "/",
                 "secure" => false,
                 "httponly" => true,
             ]);
